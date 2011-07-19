f9b899184adfb3f169527cff70f5fd5a998e2b37
S <s>
2011-07-19 17:17:11 -0400

    {"type":"SWC", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -10,6 +10,7 @@ import acm.program.*;
 import acm.util.*;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Hangman extends ConsoleProgram {
 
@@ -17,21 +18,37 @@ public class Hangman extends ConsoleProgram {
 		while (notGuessed()) { 
 			if (N_GUESSES < 0) break; 
 			printWord(); 
-			char = askLetter();
-			boolean letterInWord = checkWordForLetter();
+			askLetter();
+			checkWordForLetter();
 			
 		}
 	}
-
+    
+    public boolean notGuessed() {
+    	return false; 
+    }
+    
     public void printWord() {
+    	print("The word now looks like this: ");
     	for (int i = 0; i < keyWord.length(); i++) {
-    		
+    		print('-');
     	}
+    	println();
+    }
+    
+    public void askLetter() {
+    	letter = readLine("Guess a letter: ");
+    	guessed.add(letter);
+    }
+    
+    public boolean checkWordForLetter() {
+    	return false; 
     }
     /* Private instance variables */
     HangmanLexicon lexi = new HangmanLexicon(); 
     RandomGenerator rgen = RandomGenerator.getInstance();
     String keyWord = lexi.getWord(rgen.nextInt(0, lexi.getWordCount() - 1));
-    char letter; 
+    ArrayList<String> guessed = new ArrayList<String>();
+    String letter; 
     private static final int N_GUESSES = 8;
 }
