a334c79b8191d25c64fe3a9e1a69d4a1d0bed56b
S <s>
2011-07-19 17:24:39 -0400

    {"type":"SWC", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -31,24 +31,34 @@ public class Hangman extends ConsoleProgram {
     public void printWord() {
     	print("The word now looks like this: ");
     	for (int i = 0; i < keyWord.length(); i++) {
-    		print('-');
+    		keyWordCopy.add("-");
     	}
     	println();
     }
     
     public void askLetter() {
+    	println("You have " + N_GUESSES + " guesses left.");
     	letter = readLine("Guess a letter: ");
     	guessed.add(letter);
     }
     
     public boolean checkWordForLetter() {
-    	return false; 
+    	int dex = keyWord.indexOf(letter);
+    	if (dex == -1) {
+    		return false; 
+    	} else {
+    		return true; 
+    	}
     }
+    
     /* Private instance variables */
     HangmanLexicon lexi = new HangmanLexicon(); 
     RandomGenerator rgen = RandomGenerator.getInstance();
+    
     String keyWord = lexi.getWord(rgen.nextInt(0, lexi.getWordCount() - 1));
-    ArrayList<String> guessed = new ArrayList<String>();
     String letter; 
+    ArrayList<String> guessed = new ArrayList<String>();
+    ArrayList<String> keyWordCopy = new ArrayList<String>();
+    
     private static final int N_GUESSES = 8;
 }
