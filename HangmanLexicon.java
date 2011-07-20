cebc790f8f94cbbc27a2043e2df7c4b9d5d9946e
S <s>
2011-07-20 12:01:47 -0400

    {"type":"SWC", "runs":"0"}

--- HangmanLexicon.java
+++ HangmanLexicon.java
@@ -31,24 +31,12 @@ public class HangmanLexicon {
 	}
 /** Returns the number of words in the lexicon. */
 	public int getWordCount() {
-		return 10;
+		return dict.size();
 	}
 
 /** Returns the word at the specified index. */
 	public String getWord(int index) {
-		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
-			case 2: return "CONNOISSEUR";
-			case 3: return "DEHYDRATE";
-			case 4: return "FUZZY";
-			case 5: return "HUBBUB";
-			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
-			default: throw new ErrorException("getWord: Illegal index");
-		}
+		return dict.get(index);
 	}
 	ArrayList<String> dict = new ArrayList<String>();
 }
